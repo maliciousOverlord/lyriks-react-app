299b19aa54d7e40d031852fe1c5b56ab572d551c
tanay <tanaybutala14@gmail.com>
2022-12-26 15:12:59 +0530

    first commit

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,33 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const shazamCoreApi = createApi({
+    reducerPath: 'shazamCoreApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+        prepareHeaders: (headers) => {
+            headers.set('X-RapidAPI-Key', '3bfbe9befcmshe1fea5ef533044ep160c02jsnc28f07faf9b1');
+            return headers;
+        },
+    }),
+    endpoints: (builder) => ({
+        getTopCharts: builder.query({ query: () => '/charts/world' }),
+        getSongsByGenre: builder.query({ query: (genre) => `/charts/genre-world?genre_code=${genre}` }),
+        getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?track_id=${songid}` }),
+        getSongRelated: builder.query({ query: ({ songid }) => `/tracks/related?track_id=${songid}` }),
+        getArtistDetails: builder.query({ query: (artistId) => `/artists/details?.artist_id=${artistId}` }), //in this we dont destructure like songid as we are passing it automatically
+        getSongsByCountry: builder.query({ query: (countryCode) => `/charts/country?country_code=${countryCode}` }),
+        getSongsBySearch: builder.query({ query: (searchTerm) => `/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}` }),
+
+    }),
+});
+
+// for using it as  hook
+export const {
+    useGetTopChartsQuery,
+    useGetSongsByGenreQuery,
+    useGetSongDetailsQuery,
+    useGetSongRelatedQuery,
+    useGetArtistDetailsQuery,
+    useGetSongsByCountryQuery,
+    useGetSongsBySearchQuery,
+} = shazamCoreApi;
